efdc25c9baed2c562b7b3f46f96da05427b45ecb
鈴木泰斗 <suzukitaito@suzukitaitonoMacBook-Pro.local>
2021-04-10 17:14:39 +0900

    Change api domain

--- src/environments/environment.godzilla.ts
+++ src/environments/environment.godzilla.ts
@@ -2,5 +2,5 @@
 export const environment = {
   production: true,
   googleMapAPIKey: 'AIzaSyAATL88SybURO1USqQq6IqEPpoRNzheC7o',
-  api: 'https://s2-demo-go-7a7bdvhwoa-an.a.run.app',
+  api: 'https://api-s2-sandbox.tach.dev',
 };
